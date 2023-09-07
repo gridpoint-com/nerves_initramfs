2bfa97b000a01d055141c550ebeae3d2d197abc7
Jon Ringle <jringle@gridpoint.com>
2023-09-07 15:49:51 -0400

    change fwup to edge_firmware.fw

--- builder/board/nerves_initramfs_edge/bootstrap.sh
+++ builder/board/nerves_initramfs_edge/bootstrap.sh
@@ -3,7 +3,7 @@
 set -e
 
 mount /dev/sda1 /mnt
-fwup -ad /dev/mtdblock1 -i /mnt/ec3_firmware.fw -t complete --unsafe
+fwup -ad /dev/mtdblock1 -i /mnt/edge_firmware.fw -t complete --unsafe
 umount /mnt
 
 reboot -f
